eaa2ded4541d54e81f6757eb2b9f42c5f9e7abfc
dam2 <dam2@a223e12.dc.domenicoscarlatti.es>
2024-10-10 17:38:37 +0200

    Pruba commit

---
+++ src/proyecto_aio_flotavehiculos/Proyecto_AIO_FlotaVehiculos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_aio_flotavehiculos;
+
+/**
+ *
+ * @author dam2
+ */
+public class Proyecto_AIO_FlotaVehiculos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
